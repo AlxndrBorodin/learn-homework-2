6de5bd225275dec0677da778d3b09322a56eaa40
Alxndr Borodin <alxndr.borodin@gmail.com>
2021-05-26 19:41:53 +0300

    Second lesson

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -1,28 +1,18 @@
-"""
-Домашнее задание №2
 
-Дата и время
-
-1. Напечатайте в консоль даты: вчера, сегодня, 30 дней назад
-2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
-
-"""
+from datetime import date, datetime, timedelta
+from time import strftime
 
 def print_days():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    dt = date.today()
+    delta = timedelta(days=1)
+    dt_yesterday = dt - delta
+    dt_30days = dt - 30*delta
+    print(f'Вчера было {dt_yesterday}, сегодня - {dt}, а 30 дней назад было {dt_30days}')
 
 
 def str_2_datetime(date_string):
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
-
+    return datetime.strptime(date_string, '%d/%m/%y %H:%M:%S.%f')
+    
 if __name__ == "__main__":
     print_days()
-    print(str_2_datetime("01/01/20 12:10:03.234567"))
+    print(str_2_datetime('01/01/20 12:10:03.234567'))
